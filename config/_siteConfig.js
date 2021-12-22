5b7b5955af81f4934beff69ef8c75db7be2b3f77
Farah El Ghemary <65469880+elghemary@users.noreply.github.com>
2021-12-22 21:14:32 +0100

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -51,7 +51,7 @@ export default {
   disqus: {
     on: true,
     loadingStrategy: 'button', // Options: onload, lazy, button
-    siteShortName: 'blog-danielkelly-io' // 'blog-danielkelly-io'
+    siteShortName: 'elghemarys-blog' // 'blog-danielkelly-io'
   },
 
   // Newsletter Subscribe
